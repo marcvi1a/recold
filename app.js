e3e7922a147a5ea2fc51ed17017d42012860b675
marcvi1a <marc.vilallimos@gmail.com>
2025-11-23 15:56:23 -0300

    New

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const cameraPreview = document.getElementById("camera-preview");
-const cameraStart = document.getElementById("camera-start");
-const camera = document.getElementById("camera");
-
-cameraStart.addEventListener("click", async () => {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "user" }
-    });
-
-    camera.srcObject = stream;
-
-    // Remove overlay but DO NOT resize camera
-    cameraPreview.style.display = "none";
-
-  } catch (err) {
-    alert("Camera permission denied or unavailable.");
-    console.error(err);
-  }
-});
+// const cameraPreview = document.getElementById("camera-preview");
+// const cameraStart = document.getElementById("camera-start");
+// const camera = document.getElementById("camera");
+//
+// cameraStart.addEventListener("click", async () => {
+//   try {
+//     const stream = await navigator.mediaDevices.getUserMedia({
+//       video: { facingMode: "user" }
+//     });
+//
+//     camera.srcObject = stream;
+//
+//     // Remove overlay but DO NOT resize camera
+//     cameraPreview.style.display = "none";
+//
+//   } catch (err) {
+//     alert("Camera permission denied or unavailable.");
+//     console.error(err);
+//   }
+// });
